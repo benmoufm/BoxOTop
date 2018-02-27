3156140c7c6007a1ae1dc883872175aa1ea5890f
Mélodie <m.benmouffek@gmail.com>
2018-02-27 15:44:56 +0100

    Add rest movie object and mapper

---
+++ BoxOTop/Source/Data/MovieMapper.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMapper.swift
+//  BoxOTop
+//
+//  Created by Mélodie Benmouffek on 27/02/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct MovieMapper {
+    let restMovie: RestMovie
+
+    func map() -> Movie {
+        return Movie(id: restMovie.id, title: restMovie.title)
+    }
+}
