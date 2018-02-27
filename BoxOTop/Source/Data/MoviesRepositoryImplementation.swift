49a8714366de55ae930046a0f2af5fefe6e4bf50
Mélodie <m.benmouffek@gmail.com>
2018-02-27 15:28:09 +0100

    Add get movies from query function to movies repository

--- BoxOTop/Source/Data/MoviesRepositoryImplementation.swift
+++ BoxOTop/Source/Data/MoviesRepositoryImplementation.swift
@@ -7,7 +7,22 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class MoviesRepositoryImplementation: MoviesRepository {
+    let httpManager: HttpManager
 
+    init() {
+        let configuration = URLSessionConfiguration.default
+        let sessionManager = Alamofire.SessionManager(configuration: configuration)
+        httpManager = HttpManager(sessionManager: sessionManager)
+    }
+
+    func getMovies(with query: String,_ completion: ((Result<[Movie]>) -> Void)?) {
+        let httpRequest = HttpRequest(parameters: ["s":query])
+        httpManager.execute(httpRequest: httpRequest) { result -> Void in
+
+        }
+    }
 }
