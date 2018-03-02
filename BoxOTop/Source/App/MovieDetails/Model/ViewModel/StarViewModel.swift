9606e75082b5cf468746302f80bf774fbefdd19d
Mélodie <m.benmouffek@gmail.com>
2018-03-02 19:27:48 +0100

    Add star view class, view model and maper

---
+++ BoxOTop/Source/App/MovieDetails/Model/ViewModel/StarViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  StarViewModel.swift
+//  BoxOTop
+//
+//  Created by Mélodie Benmouffek on 02/03/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StarViewModel {
+    let fillingRatio: CGFloat
+}
